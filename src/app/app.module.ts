aef7ad97d64306994bd5417eb16bc5fc94fd0f87
Mikaela Estefan <estefanmikaela@gmail.com>
2023-10-16 22:44:20 -0300

    dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ToolbarComponent } from './dashboard/components/toolbar/toolbar.component';
+import { SidebarComponent } from './dashboard/components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ToolbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,

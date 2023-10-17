9eb763ee7eafce05897d7e3efe0a3eff72520b8b
Mikaela Estefan <estefanmikaela@gmail.com>
2023-10-16 22:59:37 -0300

    pages added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ToolbarComponent } from './dashboard/components/toolbar/toolbar.component';
 import { SidebarComponent } from './dashboard/components/sidebar/sidebar.component';
+import { CoursesComponent } from './dashboard/pages/courses/courses.component';
+import { FormsComponent } from './dashboard/forms/forms.component';
+import { HomeComponent } from './dashboard/pages/home/home.component';
+import { IconsComponent } from './dashboard/icons/icons.component';
+import { UsersComponent } from './dashboard/pages/users/users.component';
+import { UsersDialogComponent } from './users/components/users-dialog/users-dialog.component';
+import { UsersTableComponent } from './dashboard/pages/users/components/users-table/users-table.component';
+import { ModelsComponent } from './dashboard/pages/users/models/models.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     ToolbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    CoursesComponent,
+    FormsComponent,
+    HomeComponent,
+    IconsComponent,
+    UsersComponent,
+    UsersDialogComponent,
+    UsersTableComponent,
+    ModelsComponent
   ],
   imports: [
     BrowserModule,
